011fe18196b347380a1b89f2f13222ee155daca4
sasakiyudai <yudai14142@gmail.com>
2023-01-28 23:19:05 +0900

    Initial Commit

---
+++ ft_hangouts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ft_hangouts
+//
+//  Created by Yudai Sasaki on 2023/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
